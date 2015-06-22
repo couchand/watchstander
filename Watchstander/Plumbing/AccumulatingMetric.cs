12371720089515a44a3a289700b1a3fcf72bf17b
Andrew Couch <andy@couchand.com>
2015-06-22 11:19:07 -0400

    accumulating and set-once metrics

---
+++ Watchstander/Plumbing/AccumulatingMetric.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Watchstander.Common;
+
+namespace Watchstander.Plumbing
+{
+	public class AccumulatingMetric : IMetric
+	{
+		public string Name { get; }
+
+		protected string description;
+		protected Rate rate;
+		protected string unit;
+
+		public TaggerRepository TaggerRepository;
+
+		public string Description => description;
+		public Rate Rate => rate;
+		public string Unit => unit;
+		public IReadOnlyList<string> TagKeys => TaggerRepository.GetAllTagKeys().ToList().AsReadOnly();
+
+		public AccumulatingMetric (string Name)
+		{
+			this.Name = Name;
+			this.description = null;
+			this.rate = Rate.Unknown;
+			this.unit = null;
+			this.TaggerRepository = new TaggerRepository();
+		}
+
+		public virtual void SetDescription (string description)
+		{
+			this.description = description;
+		}
+
+		public virtual void SetRate (Rate rate)
+		{
+			if (rate == Rate.Unknown)
+				throw new Exception ("Must provide a valid rate!");
+
+			this.rate = rate;
+		}
+
+		public virtual void SetUnit (string unit)
+		{
+			this.unit = unit;
+		}
+	}
+
+	public class SetOnceMetric : AccumulatingMetric
+	{
+		public SetOnceMetric (string Name) : base (Name) {}
+
+		public override void SetDescription (string description)
+		{
+			if (this.description != null)
+				throw new Exception ("Can only set metric description once!");
+
+			base.SetDescription (description);
+		}
+
+		public override void SetRate (Rate rate)
+		{
+			if (this.rate != Rate.Unknown)
+				throw new Exception ("Can only set metric rate once!");
+			
+			base.SetRate (rate);
+		}
+
+		public override void SetUnit (string unit)
+		{
+			if (this.unit != null)
+				throw new Exception ("Can only set metric unit once!");
+
+			base.SetUnit (unit);
+		}
+	}
+}
+
---
+++ WatchstanderTests/Unit/AccumulatingMetricTests.cs
@@ -0,0 +1,147 @@
+﻿using NUnit;
+using NUnit.Framework;
+
+using System;
+
+using Watchstander.Common;
+using Watchstander.Plumbing;
+
+namespace WatchstanderTests.Unit
+{
+	[TestFixture]
+	public class AccumulatingMetricTests
+	{
+		[Test]
+		public void TestSetDescription ()
+		{
+			var metric = new AccumulatingMetric ("foo.bar.baz");
+
+			Assert.IsNull (metric.Description);
+
+			var wittyDescription = "The number of baz used by bar on foo.";
+
+			metric.SetDescription (wittyDescription);
+
+			Assert.AreEqual (wittyDescription, metric.Description);
+
+			metric.SetDescription ("something else");
+
+			Assert.AreEqual ("something else", metric.Description);
+		}
+
+		[Test]
+		public void TestSetRate ()
+		{
+			var metric = new AccumulatingMetric ("foo.bar.baz");
+
+			Assert.AreEqual (Rate.Unknown, metric.Rate);
+
+			metric.SetRate (Rate.Gauge);
+
+			Assert.AreEqual (Rate.Gauge, metric.Rate);
+
+			metric.SetRate (Rate.Counter);
+
+			Assert.AreEqual (Rate.Counter, metric.Rate);
+		}
+
+		[Test]
+		public void TestSetRateRequiresKnownRate ()
+		{
+			var metric = new AccumulatingMetric ("foo.bar.baz");
+
+			Assert.Throws<Exception> (() => metric.SetRate (Rate.Unknown));
+		}
+
+		[Test]
+		public void TestSetUnit ()
+		{
+			var metric = new AccumulatingMetric ("foo.bar.baz");
+
+			Assert.IsNull (metric.Unit);
+
+			metric.SetUnit ("baz");
+
+			Assert.AreEqual ("baz", metric.Unit);
+
+			metric.SetUnit ("volts");
+
+			Assert.AreEqual ("volts", metric.Unit);
+		}
+
+		[Test]
+		public void TestAddTagKey ()
+		{
+			var metric = new AccumulatingMetric ("foo.bar.baz");
+
+			Assert.AreEqual (0, metric.TagKeys.Count);
+
+			metric.TaggerRepository.AddStatic ("host", "foobar");
+
+			Assert.AreEqual (1, metric.TagKeys.Count);
+			Assert.AreEqual ("host", metric.TagKeys [0]);
+		}
+	}
+
+	[TestFixture]
+	public class SetOnceMetricTests
+	{
+		[Test]
+		public void TestSetDescription ()
+		{
+			var metric = new SetOnceMetric ("foo.bar.baz");
+
+			Assert.IsNull (metric.Description);
+
+			var wittyDescription = "The number of baz used by bar on foo.";
+
+			metric.SetDescription (wittyDescription);
+
+			Assert.AreEqual (wittyDescription, metric.Description);
+
+			Assert.Throws<Exception> (() => metric.SetDescription ("something else"));
+		}
+
+		[Test]
+		public void TestSetRate ()
+		{
+			var metric = new SetOnceMetric ("foo.bar.baz");
+
+			Assert.AreEqual (Rate.Unknown, metric.Rate);
+
+			metric.SetRate (Rate.Gauge);
+
+			Assert.AreEqual (Rate.Gauge, metric.Rate);
+
+			Assert.Throws<Exception> (() => metric.SetRate (Rate.Counter));
+		}
+
+		[Test]
+		public void TestSetUnit ()
+		{
+			var metric = new SetOnceMetric ("foo.bar.baz");
+
+			Assert.IsNull (metric.Unit);
+
+			metric.SetUnit ("baz");
+
+			Assert.AreEqual ("baz", metric.Unit);
+
+			Assert.Throws<Exception> (() => metric.SetUnit ("volts"));
+		}
+
+		[Test]
+		public void TestAddTagKey ()
+		{
+			var metric = new SetOnceMetric ("foo.bar.baz");
+
+			Assert.AreEqual (0, metric.TagKeys.Count);
+
+			metric.TaggerRepository.AddStatic ("host", "foobar");
+
+			Assert.AreEqual (1, metric.TagKeys.Count);
+			Assert.AreEqual ("host", metric.TagKeys [0]);
+		}
+	}
+}
+
