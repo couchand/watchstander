5d0422d933fbb2ff02602d3ab8531de8be6761ab
Andrew Couch <andy@couchand.com>
2015-06-22 11:44:10 -0400

    disposable container

---
+++ Watchstander/Utilities/DisposableContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Watchstander.Utilities
+{
+	public class DisposableContainer : IDisposable
+	{
+		private readonly List<IDisposable> disposables;
+
+		public DisposableContainer ()
+		{
+			disposables = new List<IDisposable> ();
+		}
+
+		public void Add(IDisposable disposable)
+		{
+			disposables.Add (disposable);
+		}
+
+		public void Dispose()
+		{
+			foreach (var disposable in disposables)
+			{
+				disposable.Dispose ();
+			}
+		}
+	}
+}
+
---
+++ WatchstanderTests/Unit/DisposableContainerTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit;
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+using Watchstander.Utilities;
+
+namespace WatchstanderTests.Unit
+{
+	class MyDisposable : IDisposable
+	{
+		private bool wasDisposed;
+
+		public bool WasDisposed => wasDisposed;
+
+		public MyDisposable()
+		{
+			wasDisposed = false;
+		}
+
+		public void Dispose()
+		{
+			wasDisposed = true;
+		}
+	}
+
+	[TestFixture]
+	public class DisposableContainerTests
+	{
+		[Test]
+		public void TestDisposeAll ()
+		{
+			var container = new DisposableContainer ();
+			var disposables = new List<MyDisposable> ();
+
+			for (int i = 0; i < 10; i++)
+			{
+				var disposable = new MyDisposable ();
+				disposables.Add (disposable);
+				container.Add (disposable);
+			}
+
+			container.Dispose ();
+
+			foreach (var disposable in disposables)
+			{
+				Assert.That (disposable.WasDisposed);
+			}
+		}
+	}
+}
+
