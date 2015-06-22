43836ff0e68b3d8b929808275cf2c107566b0e52
Andrew Couch <andy@couchand.com>
2015-06-22 11:42:02 -0400

    accumulating schema

---
+++ Watchstander/Plumbing/AccumulatingSchema.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Watchstander.Common;
+using Watchstander.Utilities;
+
+namespace Watchstander.Plumbing
+{
+	public class AccumulatingSchemaEntry : ISchemaEntry
+	{
+		private AccumulatingMetric metric;
+		public List<ITimeSeries> timeSeries;
+
+		public IMetric Metric => metric;
+		public IReadOnlyList<ITimeSeries> TimeSeries => timeSeries.AsReadOnly();
+
+		public AccumulatingSchemaEntry (AccumulatingMetric metric)
+		{
+			this.metric = metric;
+			this.timeSeries = new List<ITimeSeries>();
+		}
+
+		public void SetDescription(string description)
+		{
+			metric.SetDescription (description);
+		}
+
+		public void SetRate (Rate rate)
+		{
+			metric.SetRate (rate);
+		}
+
+		public void SetUnit (string unit)
+		{
+			metric.SetUnit (unit);
+		}
+
+		public void AddTimeSeries (ITimeSeries timeSeries)
+		{
+			this.timeSeries.Add(timeSeries);
+		}
+	}
+
+	public class AccumulatingSchema : ISchema
+	{
+		private IDictionary<string, AccumulatingSchemaEntry> entries;
+
+		public IReadOnlyDictionary<string, ISchemaEntry> Entries => (IReadOnlyDictionary<string, ISchemaEntry>)entries.AsReadOnly();
+
+		public AccumulatingSchema()
+		{
+			this.entries = new Dictionary<string, AccumulatingSchemaEntry>();
+		}
+
+		public void AddEntry(AccumulatingMetric metric)
+		{
+			this.entries [metric.Name] = new AccumulatingSchemaEntry(metric);
+		}
+	}
+}
+
